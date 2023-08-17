ea20fcd424b1c1489a051ec708bf21e624ab9eca
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2023-08-17 14:01:07 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import {v4 as uuidv4} from 'uuid';
+
+
+const PORT = process.env.PORT || 3000
+
+const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        status: "OK",
+        datetime: (new Date()).toISOString(),
+        uuid: uuidv4()
+    })
+})
+
+app.use(cors)
+
+app.use((req, res) => {
+    res.status(404)
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}`)
+})
